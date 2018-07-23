642b037ca3905237f021f767f7b8e43a0501edf1
yanqaingmiffy <1185918903@qq.com>
2018-07-23 23:39:39 +0800

    获取text list

--- utils.py
+++ utils.py
@@ -18,14 +18,26 @@ def clean_str(sentence):
     :return:
     """
     sentence = re.sub(r"[A-Za-z(),<>!?\'\`]+", " ", sentence)  # 去除英文字母和英文标点符号
-    # sentence = re.sub(r"[0-9]+", "num", sentence)  # 去除数字
+    sentence = re.sub(r"[0-9]+", "num", sentence)  # 去除数字
     # sentence = re.sub("[\s+\.\!\/_,$%^*(+\"\')]+|[+——()?【】“”！，。？、~@#￥%……&*（）]+", "", sentence)
     return sentence
 
+def get_text_list(data_path,toy):
+    with open(data_path,'r',encoding='utf-8') as f:
+        if not toy:
+            return list(map(lambda x:clean_str(x['article'].strip()),f.readlines()))
+        else:
+            return list(map(lambda x:clean_str(x['article'].strip()),f.readlines()))[:45000]
+
+
 if __name__ == '__main__':
-    with open('data/train_data.txt', 'r', encoding='utf-8') as raw_file:
-        for line in raw_file:
-            line = line.strip('\r\n')
-            data=json.loads(line.strip())
-            sen=clean_str(data['article'])
-            print(sen)
+    data_path='data/train_data.txt'
+    # with open(data_path, 'r', encoding='utf-8') as raw_file:
+    #     for line in raw_file:
+    #         line = line.strip('\r\n')
+    #         data=json.loads(line.strip())
+    #         sen=clean_str(data['article'])
+    #         print(sen)
+
+    text_list=get_text_list(data_path,toy=False)
+    print(text_list)
