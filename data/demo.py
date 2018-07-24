01a32fe50b1df703d5099c81226f2a1e1b2889d4
yanqiangmiffy <1185918903@qq.com>
2018-07-24 11:48:26 +0800

    build dataset

--- data/demo.py
+++ data/demo.py
@@ -1,8 +1,41 @@
 import json
-new_file=open('train_data.txt','w',encoding='utf-8')
-with open('train_with_summ.txt','r',encoding='utf-8') as raw_file:
-    for line in raw_file:
-        data=json.loads(line.strip('\r\n'))
-        # new_data={"summarization":data["summarization"],"article":data["article"]}
-        json_str=json.dumps(data, ensure_ascii=False)
-        new_file.write(json_str+'\n')
+import jieba
+import pandas as pd
+stop_words=open('stop_words.txt','r',encoding='utf-8').read().split('\n')
+print(stop_words)
+
+def process(filename,target_filename):
+    new_file=open(target_filename,'w',encoding='utf-8')
+    article_len=[]
+    title_len=[]
+
+    with open(filename,'r',encoding='utf-8') as raw_file:
+        for line in raw_file:
+            data=json.loads(line.strip('\r\n'))
+
+            article_words = []
+            for word in jieba.cut(data['article']):
+                if word not in stop_words:
+                    article_words.append(word)
+            article_len.append(len(article_words))
+            data['article']=' '.join(article_words)
+
+            title_words=[]
+            for word in jieba.cut(data['summarization']):
+                if word not in stop_words:
+                    title_words.append(word)
+            title_len.append(len(title_words))
+            data['summarization']=' '.join(title_words)
+
+            # new_data={"summarization":data["summarization"],"article":data["article"]}
+            json_str=json.dumps(data, ensure_ascii=False)
+
+            new_file.write(json_str+'\n')
+    article_len_df=pd.DataFrame(article_len)
+    print(article_len_df.describe())
+
+    title_len_df = pd.DataFrame(title_len)
+    print(title_len_df.describe())
+if __name__ == '__main__':
+    process('evaluation_with_ground_truth.txt','valid_data.txt')
+    process('train_with_summ.txt','train_data.txt')
--- utils.py
+++ utils.py
@@ -23,6 +23,13 @@ def clean_str(sentence):
     return sentence
 
 def get_text_list(data_path,toy,flag):
+    """
+    返回文章和标题文本列表
+    :param data_path: 文件路径
+    :param toy: 是否使用全部数据集
+    :param flag: 返回文章或者标题
+    :return:
+    """
     if flag=='article':
         with open(data_path,'r',encoding='utf-8') as f:
             if not toy:
@@ -37,10 +44,110 @@ def get_text_list(data_path,toy,flag):
                 return list(map(lambda x: clean_str(json.loads(x.strip())['summarization']), f.readlines()))[:45000]
 
 def build_dict(step,toy=False):
+    """
+    构建词典
+    :param step: 是否为训练
+    :param toy:
+    :return:
+    """
     if step=='train':
-        train_article_list=get_text_list(train_path,toy)
+        train_article_list=get_text_list(train_path,toy,flag='article')
+        train_title_list=get_text_list(train_path,toy,flag='title')
 
+        words=list()
+        for sentence in train_article_list+train_title_list:
+            for word in sentence.split(' '):
+                words.append(word)
+        word_counter=collections.Counter(words).most_common()
+        word_dict=dict()
+        word_dict["<padding>"]=0
+        word_dict["<unk>"]=1
+        word_dict["<s>"]=2
+        word_dict["</s>"]=3
+        for word,_ in word_counter:
+            word_dict[word]=len(word_dict)
 
+        with open('result/word_dict.pkl','wb') as out_data:
+            pickle.dump(word_dict,out_data,pickle.HIGHEST_PROTOCOL)
+    elif step=='valid':
+        with open('result/word_dict.pkl','rb') as in_data:
+            word_dict=pickle.load(in_data)
+
+    reversed_dict = dict(zip(word_dict.values(), word_dict.keys()))
+
+    article_max_len = 1000
+    summary_max_len = 20
+
+    return word_dict,reversed_dict,article_max_len,summary_max_len
+
+def build_dataset(step,word_dict,article_max_len,summary_max_len,toy=False):
+    """
+    创建数据集，将文本转为数字索引表示
+    :param step:
+    :param word_dict:
+    :param article_max_len: 文章最大长度
+    :param summary_max_len: 标题最大长度
+    :param toy:
+    :return:
+    """
+    if step=='train':
+        article_list=get_text_list(train_path,toy,flag='article')
+        title_list=get_text_list(train_path,toy,flag='title')
+    elif step=='valid':
+        article_list=get_text_list(valid_path,toy,flag='article')
+        title_list=get_text_list(valid_path,toy,flag='article')
+    else:
+        raise  NotImplementedError
+    x=list(map(lambda d:d.split(' '),article_list))
+    x=list(map(lambda d:list(map(lambda w:word_dict.get(w,word_dict["<unk>"]),d)),x))
+    x=list(map(lambda d:d[:article_max_len],x))
+    x=list(map(lambda d:d+(article_max_len-len(d))*[word_dict["<padding>"]],x))
+
+    y = list(map(lambda d: d.split(' '), title_list))
+    y = list(map(lambda d: list(map(lambda w: word_dict.get(w, word_dict["<unk>"]), d)), y))
+    y = list(map(lambda d: d[:(summary_max_len - 1)], y))
+
+    return x,y
+
+def batch_iter(inputs,outputs,batch_size,num_epochs):
+    """
+    生成批数据
+    :param inputs:
+    :param outputs:
+    :param batch_size:
+    :param num_epochs:
+    :return:
+    """
+    inputs=np.array(inputs)
+    outputs=np.array(outputs)
+
+    num_batches_per_epoch=(len(inputs)-1)//batch_size+1
+    for epoch in range(num_epochs):
+        for batch_num in range(num_batches_per_epoch):
+            start_index=batch_num*batch_size
+            end_index=min((batch_num+1)*batch_size,len(inputs))
+            yield inputs[start_index:end_index]
+
+def get_init_embedding(reverse_dict,embedding_size):
+    glove_file='glove/glove.300d.txt'
+    word2vec_file=get_tmpfile("word2vec.format.vec")
+    glove2word2vec(glove_file,word2vec_file)
+    print("loading Glove vectors...")
+    word2vecs=KeyedVectors.load_word2vec_format(word2vec_file)
+
+    word_vec_list=list()
+    for _,word in sorted(reverse_dict.items()):
+        try:
+            word_vec=word2vecs.word_vec(word)
+        except KeyError:
+            word_vec=np.zeros([embedding_size],dtype=np.float32)
+
+        word_vec_list.append(word_vec)
+
+    word_vec_list[2]=np.random.normal(0,1,embedding_size)
+    word_vec_list[3]=np.random.normal(0,1,embedding_size)
+
+    return np.array(word_vec_list)
 if __name__ == '__main__':
     # with open(data_path, 'r', encoding='utf-8') as raw_file:
     #     for line in raw_file:
